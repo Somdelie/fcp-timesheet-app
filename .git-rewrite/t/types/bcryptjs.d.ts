267a7ce1eadb4b3bf8580485e6cd8d6704dc9efe
Cautious Developer <cautiendlovu94@gmail.com>
2026-02-07 18:21:16 +0200

    Clean up git internals

--- .git-rewrite/t/types/bcryptjs.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "bcryptjs" {
-  // Minimal typings for the parts used in this project.
-  export function hash(
-    data: string,
-    saltOrRounds: string | number,
-  ): Promise<string>;
-  export function compare(data: string, encrypted: string): Promise<boolean>;
-}
